cb912e18df0ceea33fa2c62bef4b5bc17658fac8
mimu <mimu@mimu-mac.lan>
2019-07-27 22:21:14 +0800

    format simple-springannotation

--- simple-spring/simple-springannotation-demo/src/test/java/com/mimu/simple/sa/SpringAnnotationTest.java
+++ simple-spring/simple-springannotation-demo/src/test/java/com/mimu/simple/sa/SpringAnnotationTest.java
@@ -2,12 +2,25 @@ package com.mimu.simple.sa;
 
 
 import com.mimu.simple.sa.config.ApplicationConfig;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Arrays;
+import java.util.function.Consumer;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = ApplicationConfig.class)
 public class SpringAnnotationTest {
+
+    @Test
+    public void info() {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(ApplicationConfig.class);
+        context.refresh();
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+    }
 
 }
