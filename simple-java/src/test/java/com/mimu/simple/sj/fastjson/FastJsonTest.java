3bf71c42546a2b10b2595032a72da052f2e7dc16
mimu <mimu@mimu-mac.local>
2019-08-19 10:53:40 +0800

    fulfill project

--- simple-java/src/test/java/com/mimu/simple/sj/fastjson/FastJsonTest.java
+++ simple-java/src/test/java/com/mimu/simple/sj/fastjson/FastJsonTest.java
@@ -11,21 +11,35 @@ public class FastJsonTest {
     private static String tmpstring = "jssp1{a:a}";
 
     @Test
-    public void info(){
-        People people = new People(123,"");
+    public void info() {
+        People people = new People(123, "");
         people.setName(tmpstring);
-        System.out.println(JSONObject.toJSONString(new People(123,getjs().toJSONString())));
+        System.out.println(JSONObject.toJSONString(new People(123, getjs().toJSONString())));
         System.out.println(getjs());
         System.out.println(JSONObject.toJSON(people));
     }
 
-    public static JSONObject getjs(){
+    @Test
+    public void info1() {
         JSONObject result = new JSONObject();
-        result.put("aaa","aaaaa");
+        result.put("Wechat", "请去微信粘贴加好友我是%s，去应用市场下载【搜狐新闻】App，体验新版搜狐时刻。长按并【复制】这段话，打开【搜狐新闻】直接加我为好友");
+        result.put("QQ", "请去QQ加好友我是%s，去应用市场下载【搜狐新闻】App，体验新版搜狐时刻。长按并【复制】这段话，打开【搜狐新闻】直接加我为好友。");
+        result.put("Link", "我是%s，去应用市场下载【搜狐新闻】App，体验新版搜狐时刻。长按并【复制】这段话，打开【搜狐新闻】直接加我为好友。");
+        result.put("mobile", "我是%s，去应用市场下载【搜狐新闻】App，体验新版搜狐时刻。长按并【复制】这段话，打开【搜狐新闻】直接加我为好友。");
+        for (String key : result.keySet()) {
+            System.out.println(result.getString(key));
+            System.out.println("");
+        }
+        System.out.println(result);
+    }
+
+    public static JSONObject getjs() {
+        JSONObject result = new JSONObject();
+        result.put("aaa", "aaaaa");
         return result;
     }
 
-    static class People{
+    static class People {
         private int id;
         private String name;
 
