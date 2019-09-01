6586c8b5ea4bed2955061b9064c5e7578dba6c16
mimu <mimu@mimu-mac.lan>
2019-09-01 16:50:28 +0800

    fulfill project

--- simple-spring/simple-springhystrix-demo/src/main/java/com/mimu/simple/ssh/dao/CommonRepository.java
+++ simple-spring/simple-springhystrix-demo/src/main/java/com/mimu/simple/ssh/dao/CommonRepository.java
@@ -3,6 +3,7 @@ package com.mimu.simple.ssh.dao;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.netflix.hystrix.contrib.javanica.conf.HystrixPropertiesManager;
+import com.netflix.hystrix.exception.HystrixBadRequestException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
@@ -15,6 +16,14 @@ import org.springframework.stereotype.Repository;
 public class CommonRepository {
     private static final Logger logger = LoggerFactory.getLogger(CommonRepository.class);
 
+    /**
+     * 在HystrixCommand实现的run()方法中跑出异常时，除了HystrixBadRequestException之外，
+     * 其他异常均会被Hystrix认为命令执行失败并处罚服务降级的处理逻辑,下面的方法抛出的异常会导致主线程
+     * 异常退出
+     *
+     * @param s
+     * @return
+     */
     @HystrixCommand(fallbackMethod = "getRemoteInfoFallBack",
             commandProperties = {
                     @HystrixProperty(name = HystrixPropertiesManager.EXECUTION_ISOLATION_THREAD_TIMEOUT_IN_MILLISECONDS, value = "50"),
@@ -24,15 +33,15 @@ public class CommonRepository {
     public String getRemoteInfo(String s) {
         int random = (int) (Math.random() * 100);
         if (random > 50) {
-            throw new RuntimeException(String.valueOf(random));
+            throw new HystrixBadRequestException(String.valueOf(random));
         }
         String value = "normal logic";
         logger.info("s={},random={},value={}", s, random, value);
         return value;
     }
 
-    public String getRemoteInfoFallBack(String s,Throwable throwable) {
-        logger.error("s={},getRemoteInfo fallBack,exception=", s,throwable);
+    public String getRemoteInfoFallBack(String s, Throwable throwable) {
+        logger.error("s={},getRemoteInfo fallBack,exception=", s, throwable);
         return "getRemoteInfo fallBack";
     }
 }
