e147089528cb8b9b6634372c06a14a6c65d309d4
mimu <mimu@mimu-mac.local>
2019-09-10 18:49:22 +0800

    fulfill project

--- simple-java/src/test/java/com/mimu/simple/sj/cm/CodeInit.java
+++ simple-java/src/test/java/com/mimu/simple/sj/cm/CodeInit.java
@@ -1,113 +1,11 @@
 package com.mimu.simple.sj.cm;
 
-
-import com.github.stuxuhai.jpinyin.PinyinException;
-import com.github.stuxuhai.jpinyin.PinyinFormat;
-import com.github.stuxuhai.jpinyin.PinyinHelper;
-import jdk.nashorn.internal.ir.IfNode;
-import org.apache.commons.lang3.StringUtils;
-
-import java.awt.event.ItemEvent;
-import java.text.Collator;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Locale;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-
 /**
  * author: mimu
  * date: 2019/9/3
  */
 public class CodeInit {
-
-    private static String originCode = "123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private static char[] CODEARRY = originCode.toCharArray();
-    private static String zore11 = "00000000000";
-
-    public static String getCode(long id) {
-        StringBuilder codeBuilder = new StringBuilder();
-        while (id > 0) {
-            int mod = Math.toIntExact(id % 61);
-            id = (id - mod) / 61;
-            codeBuilder.append(CODEARRY[mod]);
-        }
-        String code = codeBuilder.toString();
-        // return code.length() < 11 ? code + zore11.substring(code.length()) : code;
-        return code;
-    }
-
-    public static long deCode(String realCode) {
-        //String realCode = code.replaceAll("0", "");
-        char[] codeArry = realCode.toCharArray();
-        //System.out.println(realCode);
-        long result = 0;
-        for (int i = 0; i < realCode.length(); i++) {
-            result += originCode.indexOf(String.valueOf(codeArry[i])) * Math.pow(61.0, (double) i);
-        }
-        return result;
-    }
-
     public static void main(String[] args) {
-        /*System.out.println(getCode(8555287229450072081l));
-        System.out.println(getCode(6290494046431780000l));
-        System.out.println(deCode("GkPYj8ROAZc"));
-        System.out.println(deCode("LZs7jxD1VO9"));*/
-        // System.out.println(deCode("d3000000000"));
-        //System.out.println(6555287229450072081l%16);
-        /*try {
-            System.out.println(PinyinHelper.toHanYuPinyinString("张1名",new HanyuPinyinOutputFormat(),"",true));
-            System.out.println(PinyinHelper.toHanYuPinyinString("$张名",new HanyuPinyinOutputFormat(),"",false));
-            System.out.println(PinyinHelper.toHanYuPinyinString("d张ming",new HanyuPinyinOutputFormat(),"",true));
-        } catch (BadHanyuPinyinOutputFormatCombination badHanyuPinyinOutputFormatCombination) {
-            badHanyuPinyinOutputFormatCombination.printStackTrace();
-        }*/
-        //System.out.println(order(getList()));
-        System.out.println(orderString(getList()));
 
     }
-
-    public static List<String> getList() {
-        List<String> name = new ArrayList<>();
-        name.add("1 黄秋龙");
-        name.add("1京秋龙");
-        name.add("$黄秋龙");
-        name.add("韩杨");
-        name.add("haoming");
-        name.add("韩$杨");
-        name.add("翔子");
-        name.add("安明");
-        name.add("褚健");
-        return name;
-    }
-
-    public static List<String> order(List<String> name) {
-        Collator collator = Collator.getInstance(Locale.CHINA);
-        return name.stream().sorted(collator::compare).collect(Collectors.toList());
-    }
-
-    public static List<String> orderString(List<String> name) {
-        Collator collator = Collator.getInstance(Locale.CHINA);
-        List<String> chinese =
-                name.stream().filter(s -> Character.isLetter(getPinYinString(s).charAt(0))).collect(Collectors.toList());
-        List<String> noneChinese = name.stream().filter(s -> !Character.isLetter(getPinYinString(s).charAt(0))).collect(Collectors.toList());
-        chinese = chinese.stream().sorted(Comparator.comparing(CodeInit::getPinYinString)).collect(Collectors.toList());
-        noneChinese = noneChinese.stream().sorted(collator::compare).collect(Collectors.toList());
-        chinese.addAll(noneChinese);
-        return chinese;
-    }
-
-    public static String getPinYinString(String china) {
-        if (StringUtils.isEmpty(china)) {
-            return "";
-        }
-        try {
-            return PinyinHelper.convertToPinyinString(china, "", PinyinFormat.WITHOUT_TONE).toLowerCase();
-        } catch (PinyinException e) {
-            return china;
-        }
-    }
-
 }
--- simple-java/src/test/java/com/mimu/simple/sj/cm/NetworkTest.java
+++ simple-java/src/test/java/com/mimu/simple/sj/cm/NetworkTest.java
@@ -1,22 +1,15 @@
 package com.mimu.simple.sj.cm;
 
 
-import org.apache.commons.lang3.StringUtils;
-
-import javax.crypto.*;
-import javax.crypto.spec.SecretKeySpec;
 import java.net.InetSocketAddress;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-
 /**
  * author: mimu
  * date: 2019/7/12
  */
 public class NetworkTest {
     public static void main(String[] args) {
-        while (true){
-            InetSocketAddress address = new InetSocketAddress("localhost",9090);
+        while (true) {
+            InetSocketAddress address = new InetSocketAddress("localhost", 9090);
             System.out.println(address.getAddress());
             try {
                 Thread.sleep(1000);
@@ -24,38 +17,5 @@ public class NetworkTest {
                 e.printStackTrace();
             }
         }
-    }
-
-    public String getDecript(String key, String decMessage) {
-        if (StringUtils.isEmpty(decMessage) || StringUtils.isEmpty(key)) {
-            return "";
-        }
-        byte[] keyByte = key.substring(0, 16).getBytes();
-        SecretKey secretKey = new SecretKeySpec(keyByte, "AES");
-        try {
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            cipher.init(Cipher.DECRYPT_MODE, secretKey);
-            byte[] decode = Hex.decode(decMessage);
-            byte[] bytes = cipher.doFinal(decode);
-            return new String(bytes);
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException | BadPaddingException | InvalidKeyException | IllegalBlockSizeException e) {
-        }
-        return "";
-    }
-
-    public String getEncript(String key, String originMessage) {
-        if (StringUtils.isEmpty(key) || StringUtils.isEmpty(originMessage)) {
-            return "";
-        }
-        byte[] keyByte = key.substring(0, 16).getBytes();
-        SecretKey secretKey = new SecretKeySpec(keyByte, "AES");
-        try {
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-            byte[] bytes = cipher.doFinal(originMessage.getBytes());
-            return Hex.encode2Str(bytes);
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException | BadPaddingException | InvalidKeyException | IllegalBlockSizeException e) {
-        }
-        return null;
     }
 }
