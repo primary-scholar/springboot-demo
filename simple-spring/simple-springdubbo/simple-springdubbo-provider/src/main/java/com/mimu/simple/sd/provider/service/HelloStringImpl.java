4806084dea6082a39bdfe57106a7ee5a73295084
mimu <mimu@mimu-mac.local>
2019-09-20 10:38:32 +0800

    fulfill project

--- simple-spring/simple-springdubbo/simple-springdubbo-provider/src/main/java/com/mimu/simple/sd/provider/service/HelloStringImpl.java
+++ simple-spring/simple-springdubbo/simple-springdubbo-provider/src/main/java/com/mimu/simple/sd/provider/service/HelloStringImpl.java
@@ -2,12 +2,19 @@ package com.mimu.simple.sd.provider.service;
 
 import com.mimu.simple.sd.api.HelloStringApi;
 import com.mimu.simple.sd.core.model.HelloData;
+import org.apache.dubbo.config.annotation.Service;
 import org.springframework.stereotype.Component;
 
 /**
  * author: mimu
  * date: 2019/9/18
  */
+
+/**
+ * 如果使用 spring-dubbo-provider-annotation.xml 文件则需 在service 类上添加
+ * dubbo @Service 注解
+ */
+@Service(interfaceClass = HelloStringApi.class, timeout = 10)
 @Component
 public class HelloStringImpl implements HelloStringApi {
     @Override
