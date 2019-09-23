57e133c8a5537afb02964c610effbc70c346016c
mimu <mimu@mimu-mac.local>
2019-09-23 17:00:15 +0800

    fulfill project

--- simple-spring/simple-springdubbo/simple-springdubbo-consumer/src/test/java/com/mimu/simple/sd/consumer/ExtensionLoaderTest.java
+++ simple-spring/simple-springdubbo/simple-springdubbo-consumer/src/test/java/com/mimu/simple/sd/consumer/ExtensionLoaderTest.java
@@ -3,6 +3,7 @@ package com.mimu.simple.sd.consumer;
 import org.apache.dubbo.common.extension.ExtensionLoader;
 import org.apache.dubbo.rpc.Protocol;
 import org.apache.dubbo.rpc.protocol.dubbo.DubboProtocol;
+import org.apache.dubbo.rpc.protocol.injvm.InjvmProtocol;
 import org.junit.Test;
 
 /**
@@ -13,8 +14,12 @@ public class ExtensionLoaderTest {
 
     @Test
     public void info(){
+        Protocol adaptiveExtension = ExtensionLoader.getExtensionLoader(Protocol.class).getAdaptiveExtension();
         ExtensionLoader<Protocol> protocolExtensionLoader = ExtensionLoader.getExtensionLoader(Protocol.class);
         Protocol dubboProtocol = protocolExtensionLoader.getExtension(DubboProtocol.NAME);
-        System.out.println(dubboProtocol.getDefaultPort());
+        System.out.println(dubboProtocol);
+
+        Protocol inJvmProtocol = protocolExtensionLoader.getExtension(InjvmProtocol.NAME);
+        System.out.println(inJvmProtocol);
     }
 }
