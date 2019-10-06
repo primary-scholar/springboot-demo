5cb0ed275e0116cc3a1b941deaf2b6f93a403c50
mmimu <primaryscholar@126.com>
2019-10-06 23:50:13 +0800

    fulfill project

--- simple-java/src/test/java/com/mimu/simple/sj/seriali/FastJsonSerializedTest.java
+++ simple-java/src/test/java/com/mimu/simple/sj/seriali/FastJsonSerializedTest.java
@@ -8,6 +8,26 @@ import org.junit.Test;
  * author: mimu
  * date: 2019/10/5
  */
+
+/**
+ * FastJson 序列化过程 JSONObject.toJSONString(people)
+ * 1. 根据object 获取其对应Class
+ * 2。根据class信息构造SerializeBeanInfo 信息
+ * 2。1 反射获取class 所有 getXXX() 或 isXXX() 方法(解析使用@JSONField的情形) 以获取序列化对应的 key
+ * 2。2 根据 2。1返回的 key 信息 利用反射获取到对象的值 以获取序列化对应的 value
+ * 3。根据 key 和 value 拼成 string 字符串
+ * <p>
+ * <p>
+ * FastJson 反序列化过程
+ * 1。String peos = "{\"aName\":\"anamela\",\"age\":11}"
+ * 1。1 JSONObject jsonObject = JSONObject.parseObject(peos)
+ * 2。People people1 = JSONObject.toJavaObject(jsonObject, People.class);
+ * <p>
+ * 1。1 首先把字符串 转换成 Map<String,Object>类型 组成 key：value 对儿
+ * 2。1 根据 class 信息构造 ObjectDeserializer 信息
+ * 2。2 反射获取class 所有 setXXX() 方法(解析使用@JSONField的情形) 以获取 反序列化后 field 接受 value 对应的 key
+ * 2。3 根据 1。1中的 Map<String,Object>的数据和 2。2 中的key进行匹配，若有对应的key 则反射method.invoke() 给field付值
+ */
 public class FastJsonSerializedTest {
 
     @Test
@@ -85,6 +105,48 @@ public class FastJsonSerializedTest {
                     "name='" + name + '\'' +
                     ", age=" + age +
                     '}';
+        }
+    }
+
+    @Test
+    public void info2() {
+        Person person = new Person("person", 11);
+        String s = JSONObject.toJSONString(person);
+        System.out.println(s);
+        JSONObject jsonObject = JSONObject.parseObject(s);
+        System.out.println(JSONObject.toJavaObject(JSONObject.parseObject(s), Person.class));
+    }
+
+    /**
+     * FastJson 是根据 getXXX() isXXX() 方法来获取key的 因此被序列化的 对象必须有 getXXX() 方法 或 isXXX() 方法
+     */
+    public static class Person {
+        private String name;
+        private int age;
+
+        public Person() {
+        }
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @JSONField(name = "bbb")
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
         }
     }
 }
