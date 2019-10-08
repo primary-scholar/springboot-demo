f13e8815a5596471de047c90a7378eec38780f15
mmimu <primaryscholar@126.com>
2019-10-08 18:04:26 +0800

    fulfill project

--- simple-spring/simple-springannotation/src/main/java/com/mimu/simple/psa/conditions/config/SimpleConditionAnnotationConfig.java
+++ simple-spring/simple-springannotation/src/main/java/com/mimu/simple/psa/conditions/config/SimpleConditionAnnotationConfig.java
@@ -14,6 +14,16 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * 可以和 springboot-demo 中 springboot-debug-running 中的 ConditionOnBean 注解进行对比
+ *
+ * @Configuration 对于 spring container  来说 被该注解标注的类是一个配置文件 类似于 xml 文件
+ * @Configuration 解析过程 该过程由 ConfigurationClassPostProcessor 来进行，解析的结果是先生成 ConfigurationClass 对象，注册到 DefaultListableBeanFactory 中,该对象中包含了 beanMethod 信息
+ * 1. 对于 使用 @Configuration 标注的嵌套 类(例如本类中的SimpleConditionAConfig) 会进行递归处理
+ * 2. 根据 @Configuration 的类定义 获取类中的是 方法 保存到 ConfigurationClass 对象中的 beanMethod中(Map<methodName，Method></>)
+ * 3. BeanDefinition 的注册过程：
+ * 根据 2.中的 configuration 类中 生成 BeanDefinition
+ * 3.1 把 beanMethod 中的 方法名当作 beanDefinitionName Method 当作 BeanDefinition 注册到 DefaultListableBeanFactory 中
+ * <p>
+ * 生成 singleton 对象时, 会根据 BeanDefinition 定义中的 @Condition 来判断是否跳过该对象的创建
  */
 @Configuration
 public class SimpleConditionAnnotationConfig {
