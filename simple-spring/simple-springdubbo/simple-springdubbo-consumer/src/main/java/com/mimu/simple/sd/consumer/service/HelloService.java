2ec1c312dacbb40832d7985d8ffc1a84ba10007b
mmimu <primaryscholar@126.com>
2019-10-14 14:20:30 +0800

    fulfill project

--- simple-spring/simple-springdubbo/simple-springdubbo-consumer/src/main/java/com/mimu/simple/sd/consumer/service/HelloService.java
+++ simple-spring/simple-springdubbo/simple-springdubbo-consumer/src/main/java/com/mimu/simple/sd/consumer/service/HelloService.java
@@ -11,10 +11,14 @@ import org.springframework.stereotype.Service;
  * date: 2019/9/18
  */
 
-
 @Service
 public class HelloService {
+
     @Reference(check = false, retries = 0,version = "*")
+    /**
+     * 可以使用直联方式 进行服务的消费，一般用于测试环境测试接口的可用性，直联方式不依赖zk的参与
+     */
+    //@Reference(check = false, retries = 0, url = "dubbo://10.2.132.47:20880?version=1.0.0")
     private HelloStringApi helloStringApi;
 
     public HelloData getHello(String name) {
