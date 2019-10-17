4c401a9cd85d84d9faeab3b2fa5422383a94ab3b
mmimu <primaryscholar@126.com>
2019-10-17 15:30:32 +0800

    fulfill project

--- simple-spring/simple-springdubbo/simple-springdubbo-consumer/src/main/java/com/mimu/simple/sd/consumer/service/CommonService.java
+++ simple-spring/simple-springdubbo/simple-springdubbo-consumer/src/main/java/com/mimu/simple/sd/consumer/service/CommonService.java
@@ -1,4 +1,3 @@
-/*
 package com.mimu.simple.sd.consumer.service;
 
 import com.mimu.simple.sd.api.UserDataApi;
@@ -6,27 +5,27 @@ import com.mimu.simple.sd.core.model.UserData;
 import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.stereotype.Service;
 
-*/
 /**
  * author: mimu
  * date: 2019/8/18
- *//*
+ */
 
 @Service
 public class CommonService {
 
-    @Reference(check = false)
     private UserDataApi userDataApi;
 
-    */
-/*@Reference(check = false)
+    @Reference(check = false, retries = 0,version = "*")
+    /**
+     * 可以使用直联方式 进行服务的消费，一般用于测试环境测试接口的可用性，直联方式不依赖zk的参与
+     */
+    //@Reference(check = false, retries = 0, url = "dubbo://10.2.132.47:20880")
     public void setUserDataApi(UserDataApi userDataApi) {
         this.userDataApi = userDataApi;
-    }*//*
+    }
 
 
     public UserData getUserData(long pid) {
         return userDataApi.getUserData(pid);
     }
 }
-*/
