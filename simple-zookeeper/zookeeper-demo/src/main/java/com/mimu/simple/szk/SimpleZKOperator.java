4f7846e39c67d484f5f824a5fb22ed4bed5083b0
mimu <mimu@mimu-mac.local>
2019-10-25 10:35:21 +0800

    fulfill project

---
+++ simple-zookeeper/zookeeper-demo/src/main/java/com/mimu/simple/szk/SimpleZKOperator.java
@@ -0,0 +1,37 @@
+package com.mimu.simple.szk;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
+import java.util.concurrent.locks.Lock;
+
+/**
+ * author: mimu
+ * date: 2019/10/24
+ */
+public class SimpleZKOperator {
+    private static volatile CuratorFramework framework;
+    private static final Object lock = new Object();
+
+    static {
+        if (framework == null) {
+            synchronized (lock) {
+                if (framework == null) {
+                    framework = CuratorFrameworkFactory.builder().
+                            connectString("localhost:2181").connectionTimeoutMs(2000)
+                            .sessionTimeoutMs(10000).retryPolicy(new ExponentialBackoffRetry(1000, 1)).namespace("customer")
+                            .build();
+                    framework.start();
+
+                }
+            }
+        }
+    }
+
+    public CuratorFramework getFramework() {
+        return framework;
+    }
+
+
+}
