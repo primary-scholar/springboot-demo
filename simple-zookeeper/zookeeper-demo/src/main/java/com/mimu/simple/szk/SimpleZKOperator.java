80c417779b738862ef294a78ff51cd2e18aa2ed3
mimu <mimu@mimu-mac.local>
2019-10-25 16:11:44 +0800

    fulfill project

--- simple-zookeeper/zookeeper-demo/src/main/java/com/mimu/simple/szk/SimpleZKOperator.java
+++ simple-zookeeper/zookeeper-demo/src/main/java/com/mimu/simple/szk/SimpleZKOperator.java
@@ -1,38 +1,75 @@
 package com.mimu.simple.szk;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.ZooDefs;
 
-import java.util.concurrent.locks.Lock;
 
 /**
  * author: mimu
  * date: 2019/10/24
  */
+@Slf4j
 public class SimpleZKOperator {
-    private static volatile CuratorFramework framework;
+    private static volatile CuratorFramework curatorClient;
     private static final Object lock = new Object();
 
     static {
-        if (framework == null) {
+        if (curatorClient == null) {
             synchronized (lock) {
-                if (framework == null) {
-                    framework = CuratorFrameworkFactory.builder().
+                if (curatorClient == null) {
+                    curatorClient = CuratorFrameworkFactory.builder().
                             connectString("localhost:2181").
                             connectionTimeoutMs(2000).
-                            sessionTimeoutMs(10000).
+                            sessionTimeoutMs(1000).
                             retryPolicy(new ExponentialBackoffRetry(1000, 1)).
                             namespace("customer").
                             build();
-                    framework.start();
+                    curatorClient.start();
                 }
             }
         }
     }
 
-    public CuratorFramework getFramework() {
-        return framework;
+
+    static class curatorCRUDOperator {
+
+        public static boolean createPNode(String path, String data) throws Exception {
+            try {
+                curatorClient.create().creatingParentsIfNeeded()
+                        .withMode(CreateMode.PERSISTENT)
+                        .withACL(ZooDefs.Ids.OPEN_ACL_UNSAFE)
+                        .forPath(path, data.getBytes());
+                return true;
+            } catch (Exception e) {
+                log.error("createPNode error", e);
+                return false;
+            }
+        }
+
+        public static String readPNode(String path) throws Exception {
+            try {
+                byte[] bytes = curatorClient.getData().forPath(path);
+                return new String(bytes);
+            } catch (Exception e) {
+                log.error("readPNode error", e);
+                return null;
+            }
+        }
+
+        public static boolean deletePNode(String path) throws Exception {
+            try {
+                curatorClient.delete().forPath(path);
+                return true;
+            } catch (Exception e) {
+                log.error("deletePNode error", e);
+                return false;
+            }
+        }
+
     }
 
 
