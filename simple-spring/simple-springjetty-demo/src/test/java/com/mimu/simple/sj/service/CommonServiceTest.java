da65eb26bc0a316fd374bc6eada0c5d6d0e6c9ed
mimu <mimu@mimu-mac.local>
2019-10-26 17:57:58 +0800

    fulfill project

---
+++ simple-spring/simple-springjetty-demo/src/test/java/com/mimu/simple/sj/service/CommonServiceTest.java
@@ -0,0 +1,18 @@
+package com.mimu.simple.sj.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * author: mimu
+ * date: 2019/10/26
+ */
+public class CommonServiceTest {
+
+    @Test
+    public void info(){
+        System.out.println("ok");
+    }
+
+}
