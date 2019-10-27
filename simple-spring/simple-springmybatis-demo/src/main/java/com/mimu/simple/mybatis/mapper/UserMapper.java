99dd30da9c60a6f2cefa1ad56a74f110ce9eebe5
mimu <mimu@mimu-mac.local>
2019-10-27 10:01:02 +0800

    fulfill project

---
+++ simple-spring/simple-springmybatis-demo/src/main/java/com/mimu/simple/mybatis/mapper/UserMapper.java
@@ -0,0 +1,23 @@
+package com.mimu.simple.mybatis.mapper;
+
+import com.mimu.simple.mybatis.dao.UserDao;
+import com.mimu.simple.mybatis.model.UserData;
+
+import java.util.List;
+
+/**
+ * author: mimu
+ * date: 2019/10/27
+ */
+public interface UserMapper {
+
+    UserData findUserById(int id);
+
+    List<UserData> listUserByName(String name);
+
+    int insertUser(UserData data);
+
+    int deleteUser(int id);
+
+    int updataUser(UserData data);
+}
