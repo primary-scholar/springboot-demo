ba7dbcdf8ade5c7e1eaab44ad7437c58269f7d2a
mimu <mimu@mimu-mac.local>
2019-10-28 15:59:04 +0800

    fulfill project

--- simple-spring/simple-springmybatis/src/main/java/com/mimu/simple/spring/mybatis/config/DataSourceConfig.java
+++ simple-spring/simple-springmybatis/src/main/java/com/mimu/simple/spring/mybatis/config/DataSourceConfig.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
