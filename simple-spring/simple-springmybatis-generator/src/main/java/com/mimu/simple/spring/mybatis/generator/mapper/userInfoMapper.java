55dceb28a09f09212e955146b5f28abdea229338
mimu <mimu@mimu-mac.local>
2019-10-30 13:10:56 +0800

    fulfill project

---
+++ simple-spring/simple-springmybatis-generator/src/main/java/com/mimu/simple/spring/mybatis/generator/mapper/userInfoMapper.java
@@ -0,0 +1,30 @@
+package com.mimu.simple.spring.mybatis.generator.mapper;
+
+import com.mimu.simple.spring.mybatis.generator.model.userInfo;
+import com.mimu.simple.spring.mybatis.generator.model.userInfoExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface userInfoMapper {
+    long countByExample(userInfoExample example);
+
+    int deleteByExample(userInfoExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(userInfo record);
+
+    int insertSelective(userInfo record);
+
+    List<userInfo> selectByExample(userInfoExample example);
+
+    userInfo selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") userInfo record, @Param("example") userInfoExample example);
+
+    int updateByExample(@Param("record") userInfo record, @Param("example") userInfoExample example);
+
+    int updateByPrimaryKeySelective(userInfo record);
+
+    int updateByPrimaryKey(userInfo record);
+}
