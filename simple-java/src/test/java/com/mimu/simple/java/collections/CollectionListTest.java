05ffd32331a289ca03b5007d4d68048b067290e8
mimu <mimu@mimu-mac.local>
2019-11-01 15:32:35 +0800

    fulfill project

--- simple-java/src/test/java/com/mimu/simple/java/collections/CollectionListTest.java
+++ simple-java/src/test/java/com/mimu/simple/java/collections/CollectionListTest.java
@@ -14,12 +14,19 @@ import java.util.Map;
 
 /**
  * LinkedList 和 ArrayList 表现功能一致，支持 增 add 删 remove 改 set 查 get
- * 都可以 存放 null
+ * 都可以 存放 null，插入的顺序==输出的顺序，线程不安全
  */
 public class CollectionListTest {
 
     /**
-     *
+     * ArrayList 使用数组 transient Object[] elementData 保存数据，
+     * 特点如下：
+     * <p>
+     * 容量不固定，随着容量的增加而动态扩容(1.5倍)（阈值基本不会达到）
+     * 有序集合（插入的顺序==输出的顺序）
+     * 插入的元素可以为null
+     * 增删改查效率更高（相对于LinkedList来说）
+     * 线程不安全
      */
     @Test
     public void arrayListInfo() {
@@ -40,6 +47,28 @@ public class CollectionListTest {
         System.out.println(arrayList);
     }
 
+    /**
+     * LinkedList 使用 双向链表 的方式 保存数据 支持头插法和尾插法两种 插入方式
+     * <p>
+     *     private static class Node<E> {
+     *         E item;
+     *         Node<E> next;
+     *         Node<E> prev;
+     *
+     *         Node(Node<E> prev, E element, Node<E> next) {
+     *             this.item = element;
+     *             this.next = next;
+     *             this.prev = prev;
+     *         }
+     *     }
+     *
+     * <p>
+     * 容量没有限制
+     * 有序集合（插入的顺序==输出的顺序）
+     * 插入的元素可以为null
+     * 增删改查效率低（相对于ArrayList来说）
+     * 线程不安全
+     */
     @Test
     public void linkedListInfo() {
         LinkedList<Integer> linkedList = new LinkedList<>();
