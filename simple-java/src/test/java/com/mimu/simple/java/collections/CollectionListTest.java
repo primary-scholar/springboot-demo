aa9be61e386fe42e778156fc27118eaf14e99a93
mimu <mimu@mimu-mac.local>
2019-11-01 11:50:23 +0800

    fulfill project

---
+++ simple-java/src/test/java/com/mimu/simple/java/collections/CollectionListTest.java
@@ -0,0 +1,51 @@
+package com.mimu.simple.java.collections;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * author: mimu
+ * date: 2019/9/23
+ */
+public class CollectionListTest {
+
+    @Test
+    public void arrayListInfo(){
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(null);
+        arrayList.add(13);
+        arrayList.add(1, 13);
+        arrayList.add(10);
+        arrayList.add(14);
+        arrayList.set(3, 11);
+
+        /**
+         * 多个 重复元素 删除第一个
+         */
+        arrayList.remove((Integer) 13);
+        arrayList.remove(3);
+        System.out.println(arrayList);
+    }
+
+    @Test
+    public void linkedListInfo() {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.add(null);
+        linkedList.add(13);
+        linkedList.add(1, 13);
+        linkedList.add(10);
+        linkedList.add(14);
+        linkedList.set(3, 11);
+
+        /**
+         * 多个 重复元素 删除第一个
+         */
+        linkedList.remove((Integer) 13);
+        linkedList.remove(3);
+        System.out.println(linkedList);
+    }
+}
