44a8b9d37d23abe71f4c4432e8707e452b3b21ad
mimu <mimu@mimu-mini.local>
2019-11-04 22:48:41 +0800

    fulfill project

--- simple-java/src/test/java/com/mimu/simple/java/cm/CompleteFutureTest.java
+++ simple-java/src/test/java/com/mimu/simple/java/cm/CompleteFutureTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.concurrent.*;
 import java.util.function.BiConsumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 /**
@@ -48,11 +49,13 @@ public class CompleteFutureTest {
     /**
      * 可以确定的是 CompletableFuture.supplyAsync() 是在一个新线程中执行 某个逻辑
      * 而 CompletableFuture.whenComplete() 是在哪个新线程中执行某个逻辑 不确定
+     *
      * @throws InterruptedException
      * @throws ExecutionException
      * @throws TimeoutException
      */
     @Test
+
     public void runAsyncInfo() throws InterruptedException, ExecutionException, TimeoutException {
         CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
             try {
@@ -62,45 +65,62 @@ public class CompleteFutureTest {
                 e.printStackTrace();
             }
             return 10;
-        }).whenCompleteAsync((integer, throwable) -> {
-            System.out.println("runAsyncInfo whenComplete thread info: " + Thread.currentThread().getName());
-            System.out.println(integer);
-        });
-        System.out.println("runAsyncInfo outer thread info:" + Thread.currentThread().getName());
-        Integer integer = future.get(100, TimeUnit.MILLISECONDS);
-        System.out.println(integer);
-    }
-
-    @Test
-    public void runAsyncInfo1() throws InterruptedException, ExecutionException, TimeoutException {
-        CompletableFuture<Object> future = CompletableFuture.supplyAsync(() -> {
-            try {
-                System.out.println("runAsyncInfo supplyAsync thread info:" + Thread.currentThread().getName());
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        }).whenComplete((integer, throwable) -> {
+            if (throwable == null) {
+                System.out.println("runAsyncInfoContrast1 whenComplete thread info: " + Thread.currentThread().getName());
+                System.out.println(integer);
+            } else {
+                System.out.println("exception occur" + throwable.getMessage());
             }
-            return null;
-        }).whenCompleteAsync((object, throwable) -> {
-            System.out.println("runAsyncInfo whenComplete thread info: " + Thread.currentThread().getName());
-            System.out.println(object);
+        }).exceptionally(throwable -> {
+            System.out.println("exception occur" + throwable.getMessage());
+            return 1;
         });
         System.out.println("runAsyncInfo outer thread info:" + Thread.currentThread().getName());
-        Object object = future.get(100, TimeUnit.MILLISECONDS);
-        System.out.println(object);
+        Integer join = future.join();
+        System.out.println(join);
     }
 
     @Test
-    public void runAsyncInfoContrast() throws InterruptedException, ExecutionException, TimeoutException {
+    public void runAsyncInfoContrast() {
         CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
             System.out.println("runAsyncInfoContrast supplyAsync thread info:" + Thread.currentThread().getName());
             return 10;
         }).whenCompleteAsync((integer, throwable) -> {
-            System.out.println("runAsyncInfoContrast whenComplete thread info: " + Thread.currentThread().getName());
-            System.out.println(integer);
+            if (throwable == null) {
+                System.out.println("runAsyncInfoContrast1 whenComplete thread info: " + Thread.currentThread().getName());
+                System.out.println(integer);
+            } else {
+                System.out.println("exception occur" + throwable.getMessage());
+            }
+        }).exceptionally(throwable -> {
+            System.out.println("exception occur" + throwable.getMessage());
+            return 1;
         });
         System.out.println("runAsyncInfoContrast outer thread info:" + Thread.currentThread().getName());
-        Integer integer = future.get(100, TimeUnit.MILLISECONDS);
-        System.out.println(integer);
+        Integer join = future.join();
+        System.out.println(join);
+    }
+
+    @Test
+    public void runAsyncInfoContrast1() throws InterruptedException, ExecutionException, TimeoutException {
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
+            System.out.println("runAsyncInfoContrast1 supplyAsync thread info:" + Thread.currentThread().getName());
+            return 10;
+        }).whenComplete((integer, throwable) -> {
+            if (throwable == null) {
+                System.out.println("runAsyncInfoContrast1 whenComplete thread info: " + Thread.currentThread().getName());
+                System.out.println(integer);
+            } else {
+                System.out.println("exception occur" + throwable.getMessage());
+            }
+        }).exceptionally(throwable -> {
+            System.out.println("exception occur" + throwable.getMessage());
+            return 1;
+        });
+        System.out.println("runAsyncInfoContrast1 outer thread info:" + Thread.currentThread().getName());
+        System.out.println(future);
+        System.out.println(future.get(20, TimeUnit.MILLISECONDS));
+        System.out.println(future);
     }
 }
