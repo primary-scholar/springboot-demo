a72b59f2c360beca2d93834f03b20715c273b7df
mimu <mimu@mimu-mac.local>
2019-11-14 12:51:16 +0800

    fulfill project

--- simple-springboot/simple-springboot-mybatis-generator/src/main/java/com/mimu/simple/springboot/mybatis/generator/SBMGApplication.java
+++ simple-springboot/simple-springboot-mybatis-generator/src/main/java/com/mimu/simple/springboot/mybatis/generator/SBMGApplication.java
@@ -5,6 +5,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * author: mimu
@@ -16,6 +17,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @MapperScan(basePackageClasses = TermInfoMapper.class)
 @SpringBootApplication
+@EnableTransactionManagement
 public class SBMGApplication {
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(SBMGApplication.class);
