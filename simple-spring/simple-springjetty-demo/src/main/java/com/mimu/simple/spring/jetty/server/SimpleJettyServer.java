97178260674d7e9f87a1d820991895a29d79f797
mimu <mimu@mimu-mac.local>
2019-11-14 17:57:05 +0800

    fulfill project

--- simple-spring/simple-springjetty-demo/src/main/java/com/mimu/simple/spring/jetty/server/SimpleJettyServer.java
+++ simple-spring/simple-springjetty-demo/src/main/java/com/mimu/simple/spring/jetty/server/SimpleJettyServer.java
@@ -24,9 +24,24 @@ public abstract class SimpleJettyServer {
 
     /**
      *  servlet 容器在启动过程中 会首先初始化 servletContext 的上下文 即 startContext() 然后初始化 servlet 容器 即 initialize()
-     *  在 startContext() 中会执行 ContextLoaderListener 的 contextInitialized() 方法，该方法中会执行 initWebApplicationContext() 方法初始化 spring 中的 AnnotationConfigWebApplicationContext() 的refresh() 方法
+     *  在 ContextHandler.startContext() 中会执行 ContextLoaderListener 的 contextInitialized() 方法，该方法中会执行 initWebApplicationContext() 方法初始化 spring 中的 AnnotationConfigWebApplicationContext() 的refresh() 方法
      *  而在 initialize() 中会执行 DispatcherServlet 的 init() 方法，在该方法中会执行 initWebApplicationContext() 方法初始化 spring 中的 AnnotationConfigWebApplicationContext() 的refresh() 方法
      *  因此 此处若指定了 ContextLoaderListener() 则 spring 中的 refresh() 方法会执行两遍 ，故此处可以 不指定 ContextLoaderListener()
+     * @throws Exception
+     */
+
+    /**
+     * 在jetty 中 各组件都 继承 lifeCycle 所以 各组件都具有生命周期 都可以启动 停止等
+     * servlet 容器 jetty 的启动过程如下:入口: server.start()-> server.doStart()
+     * 在 server.doStart() 中 jetty 会依次启动1. QueuedThreadPool;2.ServerConnector;3.ServletContextHandler;4.ErrorHandler
+     * 1.QueuedThreadPool 会初始化 ReservedThreadExecutor 并启动
+     * 2.ServerConnector
+     * 3.ServletContextHandler ServletContextHandler 继承自 ContextHandler 而在ServletContextHandler的doStart() 方法中会 执行ContextHandler.startContext()方法，
+     * 在该方法中会执行 ServletHandler 的doStart() 方法 和 ServletHandler 的 initialize() 方法
+     * ServletHandler.initialize() 中 最终会执行 DispatcherServlet 的 init() 方法 具体参考DispatcherServlet.init()(HttpServletBean 中)  DispatcherServlet->FrameworkServlet->HttpServletBean
+     * 上述过程 最终会 调用到 initWebApplicationContext() 方法
+     * 最终 会执行到 spring 中的 AnnotationConfigWebApplicationContext() 的refresh() 方法 至此 spring servlet 容器启动完毕
+     *
      * @throws Exception
      */
     public void run() throws Exception {
