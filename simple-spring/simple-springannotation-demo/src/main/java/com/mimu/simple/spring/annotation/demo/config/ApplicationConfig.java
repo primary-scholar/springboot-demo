27c2d8add67193c936026c8531a74cac1bf9a69a
mimu <mimu@mimu-mac.local>
2019-11-17 15:00:47 +0800

    fulfill project

--- simple-spring/simple-springannotation-demo/src/main/java/com/mimu/simple/spring/annotation/demo/config/ApplicationConfig.java
+++ simple-spring/simple-springannotation-demo/src/main/java/com/mimu/simple/spring/annotation/demo/config/ApplicationConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+/**
+ * author: mimu
+ * date: 2018/12/4
+ */
 @Configuration
 @ComponentScan(basePackageClasses = CommonController.class)
 @Import(value = {AspectConfig.class, DataSourceConfig.class})
