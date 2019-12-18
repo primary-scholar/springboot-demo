eb2d61673695a917944e1e17c1fc2098a384f446
mimu <mimu@mimu-mini.local>
2019-12-18 21:26:18 +0800

    modify project fulfill simple-spring model

--- simple-springboot/simple-springboot-mybatis-multipledb/src/main/java/com/mimu/simple/springboot/mybatis/multipledb/annotations/CustomMaster.java
+++ simple-springboot/simple-springboot-mybatis-multipledb/src/main/java/com/mimu/simple/springboot/mybatis/multipledb/annotations/CustomMaster.java
@@ -1,13 +1,20 @@
 package com.mimu.simple.springboot.mybatis.multipledb.annotations;
 
 /**
- author: mimu
- date: 2019/12/18
+ * author: mimu
+ * date: 2019/12/18
  */
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * here we use @CustomMaster annotation to indicate that the method which is annotated by @CustomMaster
  * will read or write the record form master dataSource
  */
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface CustomMaster {
 }
