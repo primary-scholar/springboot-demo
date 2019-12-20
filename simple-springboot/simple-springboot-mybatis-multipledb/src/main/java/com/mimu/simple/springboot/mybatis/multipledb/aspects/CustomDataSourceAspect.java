ee3f59118d8da2551ef7981ce7f98b8b7a675aff
mimu <mimu@mimu-mac.local>
2019-12-20 14:22:50 +0800

    modify CustomDataSourceAspect in simple-springboot model

--- simple-springboot/simple-springboot-mybatis-multipledb/src/main/java/com/mimu/simple/springboot/mybatis/multipledb/aspects/CustomDataSourceAspect.java
+++ simple-springboot/simple-springboot-mybatis-multipledb/src/main/java/com/mimu/simple/springboot/mybatis/multipledb/aspects/CustomDataSourceAspect.java
@@ -12,6 +12,19 @@ import org.aspectj.lang.annotation.Pointcut;
 @Aspect
 public class CustomDataSourceAspect {
 
+    /**
+     * here if we move the com.mimu.simple.springboot.mybatis.multipledb.config.AspectConfig to
+     * com.mimu.simple.springboot.mybatis.multipledb.service.AspectConfig and change the method
+     * customDataSourceAspect() to (select)(list)(get)(find)*customDataSourceAspect() in AspectConfig
+     * the CustomDataSourceAspect advisor won't be created correctly because of circular reference.
+     * why in this situation exits circular reference
+     * because
+     * the Class CustomDataSourceAspect is annotated with @Aspect,thus the spring container will generate custom advisor
+     * by AnnotationAwareAspectJAutoProxyCreator while during the instantiation of this advisor the spring realise that
+     * it should generate the proxy of CustomDataSourceAspect(wrapIfNecessary()) because of
+     * the @Pointcut() of appSlavePointcut() so spring will generate CustomDataSourceAspect again
+     * so this is the question!!!
+     */
     @Pointcut(value = "!@annotation(com.mimu.simple.springboot.mybatis.multipledb.annotations.CustomMaster)" +
             "&& (execution(* com.mimu.simple.springboot.mybatis.multipledb.service..*.select*(..))" +
             "|| execution(* com.mimu.simple.springboot.mybatis.multipledb.service..*.list*(..))" +
