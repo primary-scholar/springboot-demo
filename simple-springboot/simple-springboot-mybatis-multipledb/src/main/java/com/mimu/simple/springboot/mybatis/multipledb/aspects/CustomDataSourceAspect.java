c481c645a31c24a44861858779262ea3c3d183af
mimu <mimu@mimu-mac.local>
2019-12-23 14:52:57 +0800

    modify project change simple-springboot-mybatis-multipleddb moel

--- simple-springboot/simple-springboot-mybatis-multipledb/src/main/java/com/mimu/simple/springboot/mybatis/multipledb/aspects/CustomDataSourceAspect.java
+++ simple-springboot/simple-springboot-mybatis-multipledb/src/main/java/com/mimu/simple/springboot/mybatis/multipledb/aspects/CustomDataSourceAspect.java
@@ -14,17 +14,20 @@ import org.aspectj.lang.annotation.Pointcut;
 public class CustomDataSourceAspect {
 
     /**
-     * here if we move the com.mimu.simple.springboot.mybatis.multipledb.config.AspectConfig to
+     * here if we move the com.mimu.simple.springboot.mybatis.multipledb.service.AspectConfig to
      * com.mimu.simple.springboot.mybatis.multipledb.service.AspectConfig and change the method
-     * customDataSourceAspect() to (select)(list)(get)(find)*customDataSourceAspect() in AspectConfig
+     * findcustomDataSourceAspect() to (select)(list)(get)(find)*findcustomDataSourceAspect() in AspectConfig
      * the CustomDataSourceAspect advisor won't be created correctly because of circular reference.
      * why in this situation exits circular reference
      * because
      * the Class CustomDataSourceAspect is annotated with @Aspect,thus the spring container will generate custom advisor
-     * by AnnotationAwareAspectJAutoProxyCreator while during the instantiation of this advisor the spring realise that
-     * it should generate the proxy of CustomDataSourceAspect(wrapIfNecessary()) because of
-     * the @Pointcut() of appSlavePointcut() so spring will generate CustomDataSourceAspect again
-     * so this is the question!!!
+     * by AnnotationAwareAspectJAutoProxyCreator(wrapIfNecessary()), while during the instantiation of this advisor(spring use
+     * instantiateUsingFactoryMethod() method instanced the advisor,so spring get the factoryBean firstly(in our situation the
+     * class of factoryBean is com.mimu.simple.springboot.mybatis.multipledb.service.AspectConfig and the class is a proxy
+     * generate by spring with cglib) in the following steps spring will use proxy method to generate the advosor the steps
+     * like this: proxyMethod()->appReadDB()-> factoryMethod.findCustomDataSourceAspect() to get the aspect instance so spring
+     * will generate CustomDataSourceAspect again.
+     * here this is the question!!!
      */
     @Pointcut(value = "!@annotation(com.mimu.simple.springboot.mybatis.multipledb.annotations.CustomMaster)" +
             "&& (execution(public * com.mimu.simple.springboot.mybatis.multipledb.service..*.select*(..))" +
