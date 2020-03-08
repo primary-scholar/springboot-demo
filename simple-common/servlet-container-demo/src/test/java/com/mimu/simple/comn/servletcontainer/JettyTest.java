15e5fa33a3c428ebe99e69d2557b39a1df1858f5
mimu <yahuilu212926@sohu-inc.com>
2020-03-08 15:25:46 +0800

    modify jettyTest

--- simple-common/servlet-container-demo/src/test/java/com/mimu/simple/comn/servletcontainer/JettyTest.java
+++ simple-common/servlet-container-demo/src/test/java/com/mimu/simple/comn/servletcontainer/JettyTest.java
@@ -1,7 +1,16 @@
 package com.mimu.simple.comn.servletcontainer;
 
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerCollection;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 /**
  author: mimu
@@ -9,11 +18,25 @@ import org.eclipse.jetty.server.ServerConnector;
  */
 public class JettyTest {
     public static void main(String[] args) throws Exception {
-        int port = 9090;
-        Server server = new Server(port);
-        ServerConnector connector = new ServerConnector(server,1,1);
-        //connector.setPort(port);
-        server.addConnector(connector);
+        int port = 9091;
+        Server server = new Server();
+
+        ServerConnector connector = new ServerConnector(server);
+        connector.setPort(port);
+        server.setConnectors(new Connector[]{connector});
+
+        ServletContextHandler contextHandler = new ServletContextHandler();
+        contextHandler.setContextPath("/");
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(new AnnotationConfigWebApplicationContext());
+        ServletHolder servletHolder = new ServletHolder(dispatcherServlet);
+        contextHandler.addServlet(servletHolder, "/*");
+
+        HandlerCollection collection = new HandlerCollection();
+        collection.setHandlers(new Handler[]{contextHandler, new DefaultHandler()});
+        server.setHandler(collection);
+
         server.start();
+        server.join();
     }
+
 }
