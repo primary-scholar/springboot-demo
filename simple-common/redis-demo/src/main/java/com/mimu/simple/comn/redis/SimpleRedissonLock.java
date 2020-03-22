dc949cdf81d1202e8f2d4825c2a737ed99e6e958
mimu <yahuilu212926@sohu-inc.com>
2020-03-22 23:36:38 +0800

    modify SimpelRedissonLock

--- simple-common/redis-demo/src/main/java/com/mimu/simple/comn/redis/SimpleRedissonLock.java
+++ simple-common/redis-demo/src/main/java/com/mimu/simple/comn/redis/SimpleRedissonLock.java
@@ -1,5 +1,13 @@
 package com.mimu.simple.comn.redis;
 
+import org.redisson.Redisson;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.config.SingleServerConfig;
+
+import java.util.concurrent.TimeUnit;
+
 public class SimpleRedissonLock {
 
     private static final RedissonClient redissonClient;
@@ -7,11 +15,79 @@ public class SimpleRedissonLock {
     static {
         Config config = new Config();
         SingleServerConfig serverConfig = config.useSingleServer()
-                .setAddress()
-                .setTimeout()
-                .setConnectionPoolSize()
-                .setConnectionMinimumIdleSize();
+                .setAddress("locathost:2181")
+                .setTimeout(3000)
+                .setConnectionPoolSize(5)
+                .setConnectionMinimumIdleSize(5);
         redissonClient = Redisson.create(config);
+    }
+
+    public static RLock lock(String lockKey) {
+        RLock lock = redissonClient.getLock(lockKey);
+        lock.lock();
+        return lock;
+    }
+
+    /**
+     * 释放锁
+     * @param lockKey
+     */
+    public static void unlock(String lockKey) {
+        RLock lock = redissonClient.getLock(lockKey);
+        lock.unlock();
+    }
+
+    public static void unlock(RLock lock) {
+        lock.unlock();
+    }
+
+    /**
+     * 带超时的锁
+     * @param lockKey
+     * @param timeout 超时时间   单位：秒
+     */
+    public static RLock lock(String lockKey, int timeout) {
+        RLock lock = redissonClient.getLock(lockKey);
+        lock.lock(timeout, TimeUnit.SECONDS);
+        return lock;
+    }
+
+    /**
+     * 带超时的锁
+     * @param lockKey
+     * @param unit 时间单位
+     * @param timeout 超时时间
+     */
+    public static RLock lock(String lockKey, TimeUnit unit , int timeout) {
+        RLock lock = redissonClient.getLock(lockKey);
+        lock.lock(timeout, unit);
+        return lock;
+    }
+
+    public static boolean tryLock(String lockKey, int waitTime, int leaseTime) {
+        RLock lock = redissonClient.getLock(lockKey);
+        try {
+            return lock.tryLock(waitTime, leaseTime, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            return false;
+        }
+    }
+
+    /**
+     * 尝试获取锁
+     * @param lockKey
+     * @param unit 时间单位
+     * @param waitTime 最多等待时间
+     * @param leaseTime 上锁后自动释放锁时间
+     * @return
+     */
+    public static boolean tryLock(String lockKey, TimeUnit unit, int waitTime, int leaseTime) {
+        RLock lock = redissonClient.getLock(lockKey);
+        try {
+            return lock.tryLock(waitTime, leaseTime, unit);
+        } catch (InterruptedException e) {
+            return false;
+        }
     }
 
 
