6bda433ba3f8eea4d3862ee53f37c8186c371879
mimu <yahuilu212926@sohu-inc.com>
2020-04-28 19:14:24 +0800

    fulfill project

--- simple-spring/simple-springzookeeperinject-demo/src/main/java/com/mimu/simple/spring/szc/inject/ann/support/ZKPropertyInterceptor.java
+++ simple-spring/simple-springzookeeperinject-demo/src/main/java/com/mimu/simple/spring/szc/inject/ann/support/ZKPropertyInterceptor.java
@@ -1,18 +1,17 @@
 package com.mimu.simple.spring.szc.inject.ann.support;
 
-import org.springframework.cglib.proxy.MethodInterceptor;
-import org.springframework.cglib.proxy.MethodProxy;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
 
 import java.io.Serializable;
-import java.lang.reflect.Method;
 
 /**
  author: mimu
  date: 2020/4/28
  */
-public class ZKPropertyInterceptor extends ZKPropertyAspectSupport implements MethodInterceptor,Serializable {
+public class ZKPropertyInterceptor extends ZKPropertyAspectSupport implements MethodInterceptor, Serializable {
     @Override
-    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+    public Object invoke(MethodInvocation invocation) throws Throwable {
         return null;
     }
 }
