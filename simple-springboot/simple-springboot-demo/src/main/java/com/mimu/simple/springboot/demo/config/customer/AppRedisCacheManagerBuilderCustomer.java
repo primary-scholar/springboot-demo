c98665c261f72bc945fc1cd0b48ea1553d1a3934
mimu <yahuilu212926@sohu-inc.com>
2020-05-15 10:47:29 +0800

    modify simple-springboot-demo

--- simple-springboot/simple-springboot-demo/src/main/java/com/mimu/simple/springboot/demo/config/customer/AppRedisCacheManagerBuilderCustomer.java
+++ simple-springboot/simple-springboot-demo/src/main/java/com/mimu/simple/springboot/demo/config/customer/AppRedisCacheManagerBuilderCustomer.java
@@ -1,7 +1,17 @@
 package com.mimu.simple.springboot.demo.config.customer;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -12,24 +22,28 @@ public class AppRedisCacheManagerBuilderCustomer implements RedisCacheManagerBui
 
     @Override
     public void customize(RedisCacheManager.RedisCacheManagerBuilder builder) {
+        Map<String, RedisCacheConfiguration> customCacheConfigMap = new HashMap<>();
+        RedisCacheConfiguration minute_1_info = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMillis(RedisTTlConstant.minute_1_ttl))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(getJackson2()));
+        RedisCacheConfiguration minute_5_info = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMillis(RedisTTlConstant.minute_5_ttl))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(getJackson2()));
+        RedisCacheConfiguration minute_10_info = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMillis(RedisTTlConstant.minute_10_ttl))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(getJackson2()));
+        customCacheConfigMap.put(RedisTTlConstant.minute_1_info, minute_1_info);
+        customCacheConfigMap.put(RedisTTlConstant.minute_5_info, minute_5_info);
+        customCacheConfigMap.put(RedisTTlConstant.minute_10_info, minute_10_info);
+        builder.withInitialCacheConfigurations(customCacheConfigMap);
     }
 
 
-
-    enum RedisCacheTTLEnum {
-
-        MINUTE_1("one_minute", 60 * 1000), MINURW_5("five_minute", 5 * 60 * 1000), MINUTE_10("ten_minute", 10 * 60 * 1000);
-
-        private String desc;
-        private int ttl;
-
-        RedisCacheTTLEnum(String desc, int time) {
-            this.desc = desc;
-            this.ttl = time;
-        }
-
-        public int getTtl() {
-            return ttl;
-        }
+    private Jackson2JsonRedisSerializer<Object> getJackson2() {
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        return jackson2JsonRedisSerializer;
     }
 }
