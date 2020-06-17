2352bda376157c4b5fb5010a6c58a4ec9014eb50
mimu <mimu@mimu-mbp.local>
2020-06-17 20:05:56 +0800

    fulfill project

--- simple-springboot/simple-springboot-demo/src/main/java/com/mimu/simple/springboot/demo/job/CommonJob.java
+++ simple-springboot/simple-springboot-demo/src/main/java/com/mimu/simple/springboot/demo/job/CommonJob.java
@@ -6,23 +6,31 @@ import org.springframework.stereotype.Component;
 @Component
 public class CommonJob {
 
-    /*@Scheduled(fixedDelay = 10)
+    @Scheduled(fixedDelay = 2000)
     public void info() throws InterruptedException {
         System.out.println("fixedDela job " + System.currentTimeMillis());
         while (!biggerThan9()) {
             //System.out.println("caculate");
         }
-    }*/
+    }
 
-    @Scheduled(fixedRate = 200)
+    @Scheduled(fixedRate = 5000)
     public void print() throws InterruptedException {
-        System.out.println("fixedRate job " + System.currentTimeMillis());
+        System.out.println("fixedRate job1 " + System.currentTimeMillis());
+        while (!biggerThan9()) {
+            //System.out.println("caculate");
+        }
+    }
+
+    @Scheduled(fixedRate = 10000)
+    public void sout() throws InterruptedException {
+        System.out.println("fixedRate job2 " + System.currentTimeMillis());
         while (!biggerThan9()) {
             //System.out.println("caculate");
         }
     }
 
     private boolean biggerThan9() {
-        return Math.random() * 10 > 9.999999;
+        return Math.random() * 10 > 9;
     }
 }
