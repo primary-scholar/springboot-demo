18eb4f2c2436ac541dd536d2b5b6c6e85dd746f2
mimu <mimu@mimu-mini.local>
2020-07-04 23:24:40 +0800

    fulfill project

--- simple-common/httpclient-demo/src/main/java/com/mimu/simple/comn/http/SimpleHttpClient.java
+++ simple-common/httpclient-demo/src/main/java/com/mimu/simple/comn/http/SimpleHttpClient.java
@@ -1,17 +1,11 @@
-package com.mimu.simple.httpclient;
+package com.mimu.simple.comn.http;
 
-import com.mimu.simple.common.core.FileItem;
-import com.mimu.simple.common.util.ConvertUtil;
-import org.apache.http.Consts;
 import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.client.StandardHttpRequestRetryHandler;
@@ -58,32 +52,6 @@ public class SimpleHttpClient {
 
     public static Future<String> futureGet(String url, int connectionTimeOut, int readTimeOut) {
         return futureGet(url, null, connectionTimeOut, readTimeOut);
-    }
-
-    public static String get(String url, Object object) {
-        return get(url, object, connectionTimeOut, readTimeOut);
-    }
-
-    public static Future<String> futureGet(String url, Object object) {
-        return futureGet(url, object, connectionTimeOut, readTimeOut);
-    }
-
-    public static String get(String url, Object object, int connectionTimeOut, int readTimeOut) {
-        try {
-            return get(url, ConvertUtil.convert2Map(object), connectionTimeOut, readTimeOut);
-        } catch (Exception e) {
-            logger.error("get method error url={}", url, e);
-        }
-        return null;
-    }
-
-    public static Future<String> futureGet(String url, Object object, int connectionTimeOut, int readTimeOut) {
-        try {
-            return futureGet(url, ConvertUtil.convert2Map(object), connectionTimeOut, readTimeOut);
-        } catch (Exception e) {
-            logger.error("get method error url={}", url, e);
-            return CompletableFuture.supplyAsync(() -> null);
-        }
     }
 
     public static String get(String url, Map<String, Object> para) {
@@ -137,117 +105,7 @@ public class SimpleHttpClient {
         return result;
     }
 
-    public static String post(String url, Map<String, Object> para) {
-        return post(url, para, connectionTimeOut, readTimeOut);
-    }
-
-    public static Future<String> futurePost(String url, Map<String, Object> para) {
-        return futurePost(url, para, connectionTimeOut, readTimeOut);
-    }
-
-    public static String post(String url, Object object) {
-        return post(url, object, connectionTimeOut, readTimeOut);
-    }
-
-    public static Future<String> futurePost(String url, Object object) {
-        return futurePost(url, object, connectionTimeOut, readTimeOut);
-    }
-
-    public static String post(String url, Object object, int connectionTimeOut, int readTimeOut) {
-        try {
-            return post(url, ConvertUtil.convert2Map(object), connectionTimeOut, readTimeOut);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public static Future<String> futurePost(String url, Object object, int connectionTimeOut, int readTimeOut) {
-        try {
-            return futurePost(url, ConvertUtil.convert2Map(object), connectionTimeOut, readTimeOut);
-        } catch (Exception e) {
-            logger.error("futurePost method error url={}", url, e);
-            return CompletableFuture.supplyAsync(() -> null);
-        }
-    }
-
-    public static String post(String url, Map<String, Object> para, int connectionTimeOut, int readTimeOut) {
-        return postMethod(url, para, connectionTimeOut, readTimeOut);
-    }
-
-    public static Future<String> futurePost(String url, Map<String, Object> para, int connectionTimeOut, int readTimeOut) {
-        return CompletableFuture.supplyAsync(() -> postMethod(url, para, connectionTimeOut, readTimeOut));
-    }
-
-    private static String postMethod(String url, Map<String, Object> para, int connectionTimeOut, int readTimeOut) {
-        String result = "";
-        try {
-            URIBuilder uriBuilder = new URIBuilder(url);
-            HttpPost httpPost = new HttpPost(uriBuilder.toString());
-            if (para != null) {
-                MultipartEntityBuilder builder = MultipartEntityBuilder.create();
-                for (String paraKey : para.keySet()) {
-                    Object value = para.get(paraKey);
-                    populateParam(builder, paraKey, value);
-                }
-                HttpEntity entity = builder.build();
-                httpPost.setEntity(entity);
-            }
-            httpPost.setConfig(getRequestConfig(connectionTimeOut, readTimeOut));
-            long startTime = System.currentTimeMillis();
-            CloseableHttpResponse response = httpClient.execute(httpPost);
-            logger.info("post method url={},para={},cost={}ms", url, para, System.currentTimeMillis() - startTime);
-            HttpEntity responseEntity = response.getEntity();
-            if (responseEntity != null) {
-                result = EntityUtils.toString(responseEntity);
-            }
-            EntityUtils.consume(responseEntity);
-        } catch (IOException | URISyntaxException e) {
-            logger.error("post method url={},para={}", url, para, e);
-        }
-        return result;
-    }
-
     @SuppressWarnings("unchecked")
-    private static void populateParam(MultipartEntityBuilder builder, String key, Object value) {
-        ContentType defaultType = ContentType.create("text/plain", Consts.UTF_8);
-        if (value instanceof Byte) {
-            byte[] bytes = (byte[]) value;
-            builder.addBinaryBody(key, bytes, ContentType.DEFAULT_BINARY, key);
-        } else if (value instanceof FileItem) {
-            FileItem item = (FileItem) value;
-            builder.addBinaryBody(key, item.getBytes(), ContentType.create(item.getFileName()), item.getFileName());
-        } else if (value instanceof String) {
-            builder.addTextBody(key, value.toString(), defaultType);
-        } else if (value instanceof Number) {
-            value = String.valueOf(value);
-            builder.addTextBody(key, value.toString(), defaultType);
-        } else if (value instanceof List) {
-            List list = (List) value;
-            Object element;
-            if (list.size() > 0 && (element = list.get(0)) instanceof FileItem) {
-                FileItem item = (FileItem) element;
-                builder.addBinaryBody(key, item.getBytes(), ContentType.create(item.getFileName()), item.getFileName());
-            }
-        } else if (value instanceof Map) {
-            Map map = (Map) value;
-            Iterator<Map.Entry> iterator = map.entrySet().iterator();
-            while (iterator.hasNext()) {
-                Object nextValue = iterator.next();
-                Object iteratorValue = ((Map.Entry) nextValue).getValue();
-                if (iteratorValue instanceof List) {
-                    List listValue = (List) iteratorValue;
-                    for (Object element : listValue) {
-                        if (element instanceof FileItem) {
-                            FileItem item = (FileItem) element;
-                            builder.addBinaryBody(((Map.Entry) nextValue).getKey().toString(), item.getBytes(), ContentType.create(item.getFileName()), item.getFileName());
-                        }
-                    }
-                }
-
-            }
-        }
-    }
 
     private static void initHttpClient() {
         if (httpClient == null) {
