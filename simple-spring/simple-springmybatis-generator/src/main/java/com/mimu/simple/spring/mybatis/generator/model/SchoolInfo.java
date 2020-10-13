f1b82067a3af3bf0a2d565ccd4f393b7d3733601
mimu <mimu@mimu-mbp.local>
2020-10-13 16:12:31 +0800

    update

---
+++ simple-spring/simple-springmybatis-generator/src/main/java/com/mimu/simple/spring/mybatis/generator/model/SchoolInfo.java
@@ -0,0 +1,47 @@
+package com.mimu.simple.spring.mybatis.generator.model;
+
+import java.io.Serializable;
+
+public class SchoolInfo implements Serializable {
+    private Integer id;
+
+    private Integer serial;
+
+    private String name;
+
+    private String address;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getSerial() {
+        return serial;
+    }
+
+    public void setSerial(Integer serial) {
+        this.serial = serial;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address == null ? null : address.trim();
+    }
+}
