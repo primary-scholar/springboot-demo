55f8545f3afb8c5963622eeae8b6024fec76fa60
mimu <mimu@mimu-mbp.local>
2020-11-09 11:36:11 +0800

    update

--- simple-java/src/test/java/com/mimu/simple/java/cm/NetworkTest.java
+++ simple-java/src/test/java/com/mimu/simple/java/cm/NetworkTest.java
@@ -1,24 +1,29 @@
 package com.mimu.simple.java.cm;
 
 
+import org.junit.Before;
+import org.junit.Test;
+
 import java.net.InetSocketAddress;
+import java.util.Properties;
+
 /**
  * author: mimu
  * date: 2019/7/12
  */
 public class NetworkTest {
-    public static void main(String[] args) {
-        /*while (true) {
-            InetSocketAddress address = new InetSocketAddress("localhost", 9090);
-            System.out.println(address.getAddress());
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }*/
-        String abc="abc|abfda|";
-        System.out.println(abc.split("\\|").length);
-        //System.out.println(6401417103018471523l%128);
+
+    @Before
+    public void init(){
+        Properties properties = new Properties();
+        properties.put("a","a");
+        properties.put("b","b");
+        System.setProperties(properties);
+    }
+
+    @Test
+    public void info(){
+        System.out.println(System.getProperty("a"));
+        System.out.println(System.getProperty("b"));
     }
 }
--- simple-spring/simple-springannotation/src/main/java/com/mimu/simple/spring/annotation/autowires/ComponentA.java
+++ simple-spring/simple-springannotation/src/main/java/com/mimu/simple/spring/annotation/autowires/ComponentA.java
@@ -1,6 +1,7 @@
 package com.mimu.simple.spring.annotation.autowires;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -8,11 +9,17 @@ import javax.annotation.Resource;
 @Component
 public class ComponentA {
 
+
+    private IDependency dependency;
+
     @Autowired
-    //@Resource
-    private IDependency dependencyA;
+    @Qualifier(value = "dependencyA")
+    /*@Resource(name = "dependencyB")*/
+    public void setDependency(IDependency dependency) {
+        this.dependency = dependency;
+    }
 
     public void dependencyInfo(){
-        dependencyA.info();
+        dependency.info();
     }
 }
