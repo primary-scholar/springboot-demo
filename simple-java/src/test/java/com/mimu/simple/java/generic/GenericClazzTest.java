fa4aaed54e189a7a1a2d0c64d5925aad1e2d9da8
mimu <mimu@mimu-mbp.local>
2020-11-12 17:03:12 +0800

    update

--- simple-java/src/test/java/com/mimu/simple/java/generic/GenericClazzTest.java
+++ simple-java/src/test/java/com/mimu/simple/java/generic/GenericClazzTest.java
@@ -1,6 +1,12 @@
 package com.mimu.simple.java.generic;
 
+import com.mimu.simple.java.generic.group.ChildClass;
+import com.mimu.simple.java.generic.group.GrandChildClass;
+import com.mimu.simple.java.generic.group.ParentClass;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * author: mimu
@@ -29,6 +35,68 @@ public class GenericClazzTest {
         } catch (IllegalAccessException | InstantiationException e) {
             e.printStackTrace();
         }*/
+    }
+
+    @Test
+    public void info() {
+        List<ParentClass> parentClasses = new ArrayList<>();
+        parentClasses.add(new ParentClass());
+        parentClasses.add(new ChildClass());
+        ParentClass parentClass = parentClasses.get(0);
+
+        List<GrandChildClass> grandChildClasses = new ArrayList<>();
+        grandChildClasses.add(new GrandChildClass());
+        //grandChildClasses.add(new ParentClass());
+
+        List<? extends ChildClass> classExtends = new ArrayList<>();
+        //classExtends.add(new ParentClass());
+        ParentClass parentClass1 = classExtends.get(0);
+
+        List<? super ChildClass> classSuper = new ArrayList<>();
+        //classSuper.add(new Object());
+        //classSuper.add(new ParentClass());
+        classSuper.add(new ChildClass());
+        classSuper.add(new GrandChildClass());
+
+        Object object = classSuper.get(0);
+
+        //testExtends(parentClasses);
+        testExtends(grandChildClasses);
+        testExtends(classExtends);
+        //testExtends(classSuper);
+
+        testSuper(parentClasses);
+        //testSuper(grandChildClasses);
+        //testSuper(classExtends);
+        testSuper(classSuper);
+    }
+
+    /**
+     * 只读，不可写
+     *
+     * @param certainClass
+     */
+    public void testExtends(List<? extends ChildClass> certainClass) {
+        for (ChildClass aClass : certainClass) {
+            GrandChildClass childClass = (GrandChildClass) aClass;
+        }
+        //certainClass.add(new ChildClass());
+
+    }
+
+    /**
+     * 可读可写
+     *
+     * @param notCertainClass
+     */
+    public void testSuper(List<? super ChildClass> notCertainClass) {
+        for (Object certainClass : notCertainClass) {
+
+        }
+        //notCertainClass.add(new Object());
+        //notCertainClass.add(new ParentClass());
+        notCertainClass.add(new ChildClass());
+        notCertainClass.add(new GrandChildClass());
     }
 
 }
