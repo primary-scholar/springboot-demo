a4aa69360133fe449db3c0a6bef1d7a9ed9c2ca7
mimu <mimu@mimu-mbp.local>
2020-11-13 11:56:03 +0800

    update

--- simple-java/src/test/java/com/mimu/simple/java/generic/GenericClazzTest.java
+++ simple-java/src/test/java/com/mimu/simple/java/generic/GenericClazzTest.java
@@ -39,19 +39,50 @@ public class GenericClazzTest {
 
     @Test
     public void info() {
-        List<ParentClass> parentClasses = new ArrayList<>();
-        parentClasses.add(new ParentClass());
-        parentClasses.add(new ChildClass());
-        ParentClass parentClass = parentClasses.get(0);
+        List<ParentClass> parentClassList = new ArrayList<>();
+        List<GrandChildClass> grandChildClassList = new ArrayList<>();
+        /**
+         * 普通容器中，也只可存放 本身及其子类
+         */
+        List<ChildClass> childClassList = new ArrayList<>();
+        //childClassList.add(new Object());
+        //childClassList.add(new ParentClass());
+        childClassList.add(new ChildClass());
+        childClassList.add(new GrandChildClass());
 
-        List<GrandChildClass> grandChildClasses = new ArrayList<>();
-        grandChildClasses.add(new GrandChildClass());
-        //grandChildClasses.add(new ParentClass());
-
+        /**
+         * extends 在容器中使用时，只可读不可写
+         * 读： 默认返回 其本身(extends 后的 对象) ,可进行强制类型转换(不安全)
+         * 写：不可写
+         *
+         * 在 本例中 classExtends 可表示
+         * 1.List<ChildClass></>
+         * 2.List<GrandChildClass></>
+         * 读：从容器中读取数据时，
+         * 唯一可保证的是 获取的数据类型是 ChildClass(此时类型确定) 或其子类(此时类型不确定)
+         * 写：(不可写)
+         * 不可存放:ChildClass 因为可能是 List<GrandChildClass></>
+         * 不可存放:GrandChildClass 因为可能是 List<GrandChildClass 的兄弟类型></>
+         */
         List<? extends ChildClass> classExtends = new ArrayList<>();
-        //classExtends.add(new ParentClass());
-        ParentClass parentClass1 = classExtends.get(0);
+        //classExtends.add(new ChildClass());
+        //classExtends.add(new GrandChildClass());
+        ChildClass childClass = classExtends.get(0);
 
+        /**
+         * super 在容器中使用时，可读可写
+         * 读：默认返回 object ,可进行强制类型转换(不安全)
+         * 写：只能存放 其本身(super 后的对象)或 其子类
+         *
+         * 在本例中 classSuper 可表示
+         * 1.List<Object></>
+         * 2.List<ParentClass></>
+         * 3.List<ChildClass></>
+         * 读：从容器中读取数据时，
+         * 唯一可保证的是 获取的数据类型是 Object(此时类型确定) 或其子类(此时类型不确定)
+         * 写：
+         * 只可存放 其本身 及其子类
+         */
         List<? super ChildClass> classSuper = new ArrayList<>();
         //classSuper.add(new Object());
         //classSuper.add(new ParentClass());
@@ -60,14 +91,14 @@ public class GenericClazzTest {
 
         Object object = classSuper.get(0);
 
-        //testExtends(parentClasses);
-        testExtends(grandChildClasses);
+        //testExtends(parentClassList);
+        testExtends(childClassList);
+        testExtends(grandChildClassList);
         testExtends(classExtends);
-        //testExtends(classSuper);
 
-        testSuper(parentClasses);
-        //testSuper(grandChildClasses);
-        //testSuper(classExtends);
+        testSuper(parentClassList);
+        testSuper(childClassList);
+        //testSuper(grandChildClassList);
         testSuper(classSuper);
     }
 
