1b9e619b74895d84326c18e82a44294a14446282
mimu <mimu@mimu-mbp.local>
2020-11-24 14:46:20 +0800

    update

--- simple-java/src/test/java/com/mimu/simple/java/cm/jdk8n/JDK8FunctionTest.java
+++ simple-java/src/test/java/com/mimu/simple/java/cm/jdk8n/JDK8FunctionTest.java
@@ -3,15 +3,19 @@ package com.mimu.simple.java.cm.jdk8n;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
- author: mimu
- date: 2019/11/27
+ * author: mimu
+ * date: 2019/11/27
  */
 public class JDK8FunctionTest {
 
-    private List<Integer> parameterList;
+    private final List<Integer> parameterList = new ArrayList<>();
 
     @Before
     public void populate() {
@@ -20,5 +24,11 @@ public class JDK8FunctionTest {
         }
     }
 
-
+    @Test
+    public void info() {
+        Map<Integer, Object> collect = parameterList.stream().collect(Collectors.toMap(Function.identity(), Function.identity()));
+        System.out.println(collect);
+        Map<String, Object> collect1 = parameterList.stream().collect(Collectors.toMap(String::valueOf, Function.identity()));
+        System.out.println(collect1);
+    }
 }
