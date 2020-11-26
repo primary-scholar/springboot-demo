9382523e96b46ec0fe2916d183324232076098b3
mimu <mimu@mimu-mbp.local>
2020-11-26 16:14:21 +0800

    update

--- simple-java/src/test/java/com/mimu/simple/java/thread/ThreadPoolRelevantTest.java
+++ simple-java/src/test/java/com/mimu/simple/java/thread/ThreadPoolRelevantTest.java
@@ -19,6 +19,16 @@ public class ThreadPoolRelevantTest {
         threadPool.shutdown();
     }
 
+    /**
+     * 线程池 抛异常时的 执行清情况
+     * <p>
+     * 当执行方式是execute时,可以看到堆栈异常的输出。
+     * 当执行方式是submit时,堆栈异常没有输出。但是调用Future.get()方法时，可以捕获到异常。
+     * 不会影响线程池里面其他线程的正常执行。
+     * 线程池会把这个线程移除掉，并创建一个新的线程放到线程池中。 该逻辑在 runWorker() 中
+     *
+     * @param num
+     */
     private static void sayHi(int num) {
         System.out.println(Thread.currentThread());
         if (num % 2 == 0)
