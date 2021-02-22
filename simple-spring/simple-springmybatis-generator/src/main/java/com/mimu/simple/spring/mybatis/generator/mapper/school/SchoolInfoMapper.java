f9e319efaacf8ef6b50d6f6ae8e1b5f3bd571cd2
mi mu <mimu@mimu-mini.local>
2021-02-22 22:58:45 +0800

    format

---
+++ simple-spring/simple-springmybatis-generator/src/main/java/com/mimu/simple/spring/mybatis/generator/mapper/school/SchoolInfoMapper.java
@@ -0,0 +1,61 @@
+package com.mimu.simple.spring.mybatis.generator.mapper.school;
+
+import com.mimu.simple.spring.mybatis.generator.model.school.SchoolInfo;
+import com.mimu.simple.spring.mybatis.generator.model.school.SchoolInfoExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectKey;
+import org.apache.ibatis.annotations.Update;
+
+public interface SchoolInfoMapper {
+    long countByExample(SchoolInfoExample example);
+
+    int deleteByExample(SchoolInfoExample example);
+
+    @Delete({
+        "delete from school_info",
+        "where id = #{id,jdbcType=INTEGER}"
+    })
+    int deleteByPrimaryKey(Integer id);
+
+    @Insert({
+        "insert into school_info (serial, name, ",
+        "address)",
+        "values (#{serial,jdbcType=INTEGER}, #{name,jdbcType=VARCHAR}, ",
+        "#{address,jdbcType=VARCHAR})"
+    })
+    @SelectKey(statement="SELECT LAST_INSERT_ID()", keyProperty="id", before=false, resultType=Integer.class)
+    int insert(SchoolInfo record);
+
+    int insertSelective(SchoolInfo record);
+
+    List<SchoolInfo> selectByExample(SchoolInfoExample example);
+
+    @Select({
+        "select",
+        "id, serial, name, address",
+        "from school_info",
+        "where id = #{id,jdbcType=INTEGER}"
+    })
+    @ResultMap("com.mimu.simple.spring.mybatis.generator.mapper.school.SchoolInfoMapper.BaseResultMap")
+    SchoolInfo selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") SchoolInfo record, @Param("example") SchoolInfoExample example);
+
+    int updateByExample(@Param("record") SchoolInfo record, @Param("example") SchoolInfoExample example);
+
+    int updateByPrimaryKeySelective(SchoolInfo record);
+
+    @Update({
+        "update school_info",
+        "set serial = #{serial,jdbcType=INTEGER},",
+          "name = #{name,jdbcType=VARCHAR},",
+          "address = #{address,jdbcType=VARCHAR}",
+        "where id = #{id,jdbcType=INTEGER}"
+    })
+    int updateByPrimaryKey(SchoolInfo record);
+}
---
+++ simple-spring/simple-springmybatis-generator/src/main/java/com/mimu/simple/spring/mybatis/generator/model/student/ClassInfo.java
@@ -0,0 +1,37 @@
+package com.mimu.simple.spring.mybatis.generator.model.student;
+
+import java.io.Serializable;
+
+public class ClassInfo implements Serializable {
+    private Integer id;
+
+    private String academy;
+
+    private String grade;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAcademy() {
+        return academy;
+    }
+
+    public void setAcademy(String academy) {
+        this.academy = academy == null ? null : academy.trim();
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade == null ? null : grade.trim();
+    }
+}
