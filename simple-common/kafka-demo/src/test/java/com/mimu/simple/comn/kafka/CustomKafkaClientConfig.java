53219090790a070c750633665324423b492508b2
mimu <primaryscholar@126.com>
2022-12-24 10:53:27 +0800

    feat(add rocketmq demo): add rocketmq demo
    add rocketmq demo

--- simple-common/kafka-demo/src/test/java/com/mimu/simple/comn/kafka/CustomKafkaClientConfig.java
+++ simple-common/kafka-demo/src/test/java/com/mimu/simple/comn/kafka/CustomKafkaClientConfig.java
@@ -16,7 +16,7 @@ public class CustomKafkaClientConfig {
 
     public Properties getConsumePro() {
         Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.31.100:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.3.142:9092");
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
         props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
         props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "50000");
@@ -33,7 +33,7 @@ public class CustomKafkaClientConfig {
 
     public Properties getProducePro() {
         Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.31.100:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.3.142:9092");
         props.put(ProducerConfig.ACKS_CONFIG, "-1");
         props.put(ProducerConfig.RETRIES_CONFIG, "1");
         props.put(ProducerConfig.BATCH_SIZE_CONFIG, "8192");
